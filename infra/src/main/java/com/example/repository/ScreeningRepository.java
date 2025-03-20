369f41dcfbd9893b48ab19143772a1b54c53bcbd
LAPTOP-K37MMFLI\stran <strangehoon2@gmail.com>
2025-03-21 04:02:50 +0900

    refactor : n+1 문제 해결

--- infra/src/main/java/com/example/repository/ScreeningRepository.java
+++ infra/src/main/java/com/example/repository/ScreeningRepository.java
@@ -2,10 +2,13 @@ package com.example.repository;
 
 import com.example.entity.Screening;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface ScreeningRepository extends JpaRepository<Screening, Long> {
 
-    List<Screening> findByTheaterId(Long theaterId);
+    @Query("SELECT s FROM Screening s JOIN FETCH s.movie JOIN FETCH s.theater WHERE s.theater.id = :theaterId")
+    List<Screening> findAllByTheaterIdWithMovieAndTheater(@Param("theaterId") int theaterId);
 }
