87b0ab9d043798b90b94b7617ed6fbce5d56f248
LAPTOP-K37MMFLI\stran <strangehoon2@gmail.com>
2025-03-26 19:41:02 +0900

    feat : genre 예시 추가

--- infra/src/main/java/com/example/enums/Genre.java
+++ infra/src/main/java/com/example/enums/Genre.java
@@ -10,7 +10,14 @@ public enum Genre {
     ROMANCE("로맨스", "ROMANCE"),
     HORROR("호러", "HORROR"),
     SF("SF", "SF"),
-    DRAMA("드라마", "DRAMA"); // ✅ 추가
+    DRAMA("드라마", "DRAMA"),
+    COMEDY("코미디", "COMEDY"),
+    THRILLER("스릴러", "THRILLER"),
+    FANTASY("판타지", "FANTASY"),
+    MYSTERY("미스터리", "MYSTERY"),
+    ADVENTURE("어드벤처", "ADVENTURE"),
+    CRIME("범죄", "CRIME"),
+    ANIMATION("애니메이션", "ANIMATION");
 
     private final String value;
     private final String code;
