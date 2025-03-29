2f1a3675a39c0f72238f768502ebbb842f7b931e
LAPTOP-K37MMFLI\stran <strangehoon2@gmail.com>
2025-03-30 02:31:09 +0900

    fix : 잘못 지정된 엔티티 타입을 Movie로 수정

--- infra/src/main/java/com/example/repository/MovieRepository.java
+++ infra/src/main/java/com/example/repository/MovieRepository.java
@@ -1,7 +1,7 @@
 package com.example.repository;
 
-import com.example.entity.Screening;
+import com.example.entity.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MovieRepository extends JpaRepository<Screening, Long>, MovieRepositoryCustom{
+public interface MovieRepository extends JpaRepository<Movie, Long>, MovieRepositoryCustom{
 }
