c5d70734e98cd9a080d9fea8535e44ed738cc79f
LAPTOP-K37MMFLI\stran <strangehoon2@gmail.com>
2025-04-02 15:29:00 +0900

    feat : Pessimistic Lock 구현

--- infra/src/main/java/com/example/repository/ReservationRepository.java
+++ infra/src/main/java/com/example/repository/ReservationRepository.java
@@ -1,7 +1,11 @@
 package com.example.repository;
 
 import com.example.entity.Reservation;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -11,4 +15,7 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long> 
 
     List<Reservation> findByScreeningIdAndScreeningSeatIdIn(Long screeningId, List<Long> seatIds);
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT r FROM Reservation r WHERE r.screening.id = :screeningId AND r.screeningSeat.id IN :seatIds")
+    List<Reservation> findByScreeningIdAndScreeningSeatIdInWithLock(@Param("screeningId") Long screeningId, @Param("seatIds") List<Long> seatIds);
 }
