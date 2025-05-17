c11b0126fdba63c269ec4a3b20ac2cb4144031e0
LAPTOP-K37MMFLI\stran <strangehoon2@gmail.com>
2025-05-18 03:06:23 +0900

    setting : redis ttl 변경 #6

--- infra/src/main/java/com/example/redis/config/RedisCacheConfig.java
+++ infra/src/main/java/com/example/redis/config/RedisCacheConfig.java
@@ -31,8 +31,7 @@ public class RedisCacheConfig {
         objectMapper.activateDefaultTyping(ptv, ObjectMapper.DefaultTyping.NON_FINAL);
 
         RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
-                //.entryTtl(Duration.ofDays(1))
-                .entryTtl(Duration.ofMinutes(10))
+                .entryTtl(Duration.ofMinutes(5))
                 .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
                 .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer(objectMapper)));
 
