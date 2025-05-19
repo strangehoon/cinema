0b67b61cbb6b34867d995045c4135dcf7094a053
LAPTOP-K37MMFLI\stran <strangehoon2@gmail.com>
2025-05-19 18:17:00 +0900

    setting : per로 변환 #6

--- domain/src/main/java/com/example/movie/service/MovieService.java
+++ domain/src/main/java/com/example/movie/service/MovieService.java
@@ -14,7 +14,6 @@ import com.example.db.repository.MovieRepository;
 import com.example.movie.dto.response.MovieCreateServiceResponse;
 import com.example.movie.dto.response.MovieUpdateServiceResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -76,12 +75,11 @@ public class MovieService {
         return movie.getId();
     }
 
-    @Cacheable(
-            value = "movies",
+    @PERCacheable(
             key = "#genre != null ? #genre + '_page_' + #page : 'all_page_' + #page",
             condition = "(#genre != null and #title == null and #page >= 0 and #page < 2) " +
                     "|| (#genre == null and #title == null and #page >= 0 and #page < 2)",
-            cacheManager = "contentCacheManager"
+            ttl = 300
     )
     @Transactional(readOnly = true)
     public PageResponse<MovieScreeningServiceResponse> getMoviesWithScreenings(String title, String genre, int page, int size) {
