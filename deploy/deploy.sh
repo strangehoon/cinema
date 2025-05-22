c2f886f95abe38f1cf3e4f401c3ccc3b1817feef
LAPTOP-K37MMFLI\stran <strangehoon2@gmail.com>
2025-05-23 04:12:18 +0900

    fix : deploy.sh error 해결 #16

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -50,7 +50,7 @@ echo "🟡 새 컨테이너 실행됨 → 헬스체크 시작..."
 # 헬스체크
 for i in {1..10}; do
   sleep 2
-  STATUS_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:$NEXT_PORT/)
+  STATUS_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:$NEXT_PORT/actuator/health)
   echo "🔎 응답 코드: $STATUS_CODE"
   if [ "$STATUS_CODE" -eq 200 ]; then
     echo "✅ 헬스체크 통과"
