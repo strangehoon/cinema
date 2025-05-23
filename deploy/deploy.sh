f8999b017255d5b2287e469a7fc019004de562ac
LAPTOP-K37MMFLI\stran <strangehoon2@gmail.com>
2025-05-24 07:38:29 +0900

    fix : deploy.sh 수정 #16

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -72,7 +72,8 @@ sudo nginx -s reload
 echo "✅ Nginx 설정 적용 및 reload 완료"
 
 # 이전 컨테이너 제거
-# docker rm -f $OLD_NAME > /dev/null 2>&1 || true
+sleep 10 # 👈 여유를 조금 주자
+docker rm -f $OLD_NAME > /dev/null 2>&1 || true
 
 # 임시 파일 제거
 rm temp-compose.yml
