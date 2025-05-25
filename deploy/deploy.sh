e52f16f52fa3334cd9939755946671c962fa6958
LAPTOP-K37MMFLI\stran <strangehoon2@gmail.com>
2025-05-25 19:25:58 +0900

    refactor : deploy.sh 수정 #16

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -53,7 +53,7 @@ for i in {1..90}; do
     echo "✅ 헬스체크 통과"
     break
   fi
-  if [ $i -eq 30 ]; then
+  if [ $i -eq 45 ]; then
     echo "❌ 헬스체크 실패. 새 컨테이너 중단하지 않음. 로그 확인 필요"
     echo "📦 로그 보기: docker logs $NEXT_NAME"
     echo "📦 상태 보기: docker ps -a"
