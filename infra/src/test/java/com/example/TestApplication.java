b171967edd4f30e8bf5a5605a7112d99c0a606ac
LAPTOP-K37MMFLI\stran <strangehoon2@gmail.com>
2026-06-18 19:57:09 +0900

    refactor : 멀티모듈을 단일 모듈로 리팩토링 #7

--- infra/src/test/java/com/example/TestApplication.java
+++
@@ -1,9 +0,0 @@
-package com.example;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootApplication
-public class TestApplication {
-    public void contextLoads(){}
-}
---
+++ src/test/java/com/example/config/IntegrationRepositoryTest.java
@@ -0,0 +1,31 @@
+package com.example.config;
+
+import com.example.db.repository.*;
+import com.example.querydsl.config.QuerydslConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@DataJpaTest
+@Import(QuerydslConfig.class)
+public abstract class IntegrationRepositoryTest {
+
+    @Autowired
+    protected ReservationRepository reservationRepository;
+    @Autowired
+    protected UserRepository userRepository;
+    @Autowired
+    protected MovieRepository movieRepository;
+    @Autowired
+    protected TheaterRepository theaterRepository;
+    @Autowired
+    protected ScreeningRepository screeningRepository;
+    @Autowired
+    protected ScreeningSeatRepository screeningSeatRepository;
+    @Autowired
+    protected MovieRepositoryImpl movieRepositoryImpl;
+    @Autowired
+    protected PaymentRepository paymentRepository;
+}
